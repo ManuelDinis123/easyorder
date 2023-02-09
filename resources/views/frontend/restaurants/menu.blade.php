7ad00e7e51e2e817734e4d9ae4187e66c410f0dc
ManuelDinis <4228@feppv.pt>
2023-02-09 21:35:42 -0100

    - list view of menu items

--- resources/views/frontend/restaurants/menu.blade.php
+++ resources/views/frontend/restaurants/menu.blade.php
@@ -21,31 +21,67 @@
                         <hr>
                     </div>
                     <div class="col-6">
-                        <span class="menu-card-ico"><i class="fa-regular fa-list"></i></span>
-                        <span class="menu-card-ico"><i class="fa-regular fa-table"></i></span>
+                        <span class="menu-card-ico" id="li-view"><i class="fa-regular fa-list"></i></span>
+                        <span class="menu-card-ico choosen" id="crd-view"><i class="fa-regular fa-table"></i></span>
                     </div>
                 </div>
             </div>
             <div class="menu-card-body">
-                <div class="row">
-                    @foreach ($items as $item)
-                        <style>
-                            #item{{ $item['id'] }} {
-                                background: linear-gradient(180deg, rgba(0, 0, 0, 0) 47.4%, #000000 100%), url("{{ $item['imageUrl'] }}");
-                                background-size: cover;
-                                background-position: center;
-                                border-radius: 31px;
-                            }
-                        </style>
-                        <div class="item-card" id="item{{ $item['id'] }}">
-                            <div class="row item-cnts g-0">
-                                <div class="col-10"><span class="item-name">{{ $item['name'] }}</span></div>
-                                <div class="col-2"><span class="item-price">{{ $item['price'] + 0 }}€</span></div>
-                            </div>
-                        </div><br>
-                    @endforeach
+                <div class="card-view" id="card_view">
+                    <div class="row">
+                        @foreach ($items as $item)
+                            <style>
+                                #item{{ $item['id'] }} {
+                                    background: linear-gradient(180deg, rgba(0, 0, 0, 0) 47.4%, #000000 100%), url("{{ $item['imageUrl'] }}");
+                                    background-size: cover;
+                                    background-position: center;
+                                    border-radius: 31px;
+                                }
+                            </style>
+                            <div class="item-card" id="item{{ $item['id'] }}">
+                                <div class="row item-cnts g-0">
+                                    <div class="col-10"><span class="item-name">{{ $item['name'] }}</span></div>
+                                    <div class="col-2"><span class="item-price">{{ $item['price'] + 0 }}€</span></div>
+                                </div>
+                            </div><br>
+                        @endforeach
+                    </div>
+                </div>
+                <div class="list-view hide-view visually-hidden" id="list_view">
+                    <div class="list-item">
+                        @foreach ($items as $item)
+                            <li class="menu-item">
+                                <div class="menu-item-info">
+                                    <div class="menu-item-name">{{ $item['name'] }}</div>
+                                    <div class="menu-item-description text-muted">{{ $item['description'] }}</div>
+                                </div>
+                                <span class="menu-price">{{ $item['price'] }}€</span>
+                            </li>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 @stop
+
+<script>
+    function changeTab(id, view, idrem, viewrem) {
+        if (!$("#" + view).hasClass("hide-view visually-hidden")) return;
+
+        $("#" + viewrem).addClass("hide-view visually-hidden");
+        $("#" + idrem).removeClass("choosen");
+        $("#" + id).addClass("choosen");
+        $("#" + view).removeClass("hide-view visually-hidden");
+    }
+
+    $(document).ready(() => {
+        $(".menu-card-ico").on('click', function() {
+            if (this.id == "crd-view") {
+                changeTab("crd-view", "card_view", "li-view", "list_view");
+            } else {
+                changeTab("li-view", "list_view", "crd-view", "card_view");
+            }
+        })
+    });
+</script>
