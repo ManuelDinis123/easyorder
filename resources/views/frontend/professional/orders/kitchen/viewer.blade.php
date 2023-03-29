89df18c471803ab730610707cfa63457888bc653
ManuelDinis <4228@feppv.pt>
2023-03-29 22:24:02 +0000

    - Animation when new orders appear

--- resources/views/frontend/professional/orders/kitchen/viewer.blade.php
+++ resources/views/frontend/professional/orders/kitchen/viewer.blade.php
@@ -15,36 +15,46 @@
         let time;
         socket.on('orders', (data) => {
             if (Object.keys(ordersData).length != Object.keys(data).length) {
-                $("#orders div").remove();
-                let order_data = data;
+                // $("#orders div").remove();
                 $.each(data, (key, orders) => {
-                    $("#orders").append(
-                        "<div class='card mt-5 bounce'><div class='header'><h1>Pedido " + key +
-                        "</h1><hr style=\"width: 200px\"></div><div class='body' id='b" +
-                        key + "'></div>\
-                        <div class='footer' id='f" + key + "'></div></div>");
+                    if (ordersData[key] && data[key]) {
+                        if(Object.keys(ordersData).length > Object.keys(data).length){
+                            var removed = Object.keys(ordersData).filter(x => Object.keys(data).includes(x));
+                            $.each(removed, (k, v)=>{
+                                $("#order_"+v).remove();
+                            })
+                        }
+                    } else {
+                        $("#orders").append(
+                            "<div class='card mt-5 bounce' id='order_"+key+"'><div class='header'><h1>Pedido " +
+                            key +
+                            "</h1><hr style=\"width: 200px\"></div><div class='body' id='b" +
+                            key + "'></div>\
+                            <div class='footer' id='f" + key + "'></div></div>");
 
-                    $.each(orders, (k2, items) => {
-                        var side_items = "";
-                        $.each(Object.keys(items), (k3, okeys) => {
-                            if (okeys != "name" && okeys != "id" && okeys !=
-                                "quantity" && okeys != "deadline") {
-                                side_items += items[okeys]['quantity'] + "x " +
-                                    items[okeys]['name'] + " / ";
-                            }
+                        $.each(orders, (k2, items) => {
+                            var side_items = "";
+                            $.each(Object.keys(items), (k3, okeys) => {
+                                if (okeys != "name" && okeys != "id" && okeys !=
+                                    "quantity" && okeys != "deadline") {
+                                    side_items += items[okeys]['quantity'] +
+                                        "x " +
+                                        items[okeys]['name'] + " / ";
+                                }
+                            })
+                            side_items = side_items.slice(0, -2);
+                            $("#b" + key).append("<div class=\"item\">\
+                                <span class=\"title\">" + items.quantity + "x " + items.name + ".</span>\
+                                <span class=\"sides\">" + side_items + "</span>\
+                                </div><hr style=\"width: 50vw\">");
+
+                            let time = new Date(items.deadline);
+                            $("#f" + key + " h5").remove();
+                            $("#f" + key).append(
+                                "<h5 class=\"title\" style=\"float: right\">" + time
+                                .getHours() + ":" + time.getMinutes() + "</h5>");
                         })
-                        side_items = side_items.slice(0, -2);
-                        $("#b" + key).append("<div class=\"item\">\
-                            <span class=\"title\">" + items.quantity + "x " + items.name + ".</span>\
-                            <span class=\"sides\">" + side_items + "</span>\
-                            </div><hr style=\"width: 50vw\">");
-
-                        let time = new Date(items.deadline);
-                        $("#f" + key + " h5").remove();
-                        $("#f" + key).append(
-                            "<h5 class=\"title\" style=\"float: right\">" + time
-                            .getHours() + ":" + time.getMinutes() + "</h5>");
-                    })
+                    }
                 });
                 ordersData = data;
             }
