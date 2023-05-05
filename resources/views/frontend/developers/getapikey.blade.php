88c90e930a8198fa667d8abe6356be7449d85b2c
Manuel Dinis <4228@feppv.pt>
2023-05-05 17:08:38 +0000

    - Generating api keys

--- resources/views/frontend/developers/getapikey.blade.php
+++ resources/views/frontend/developers/getapikey.blade.php
@@ -6,7 +6,6 @@
 
 @section('content')
     @include('layouts.professional.tabs', ['tab' => 'dev'])
-
     <div class="container">
         <div class="center">
             <div class="getApiKey">
@@ -14,12 +13,43 @@
                     <h1><i class="fa-solid fa-code"></i> Gerar API Key</h1>
                 </div>
                 <hr>
-                <button class="btn btn-dark form-control">Gerar uma API Key</button>
+                <button class="btn btn-dark form-control" id="generate">Gerar uma API Key</button>
                 <div class="center mt-3">
-                    <span style="font-size: 24px">*****</span>
+                    <input class="form-control" style="font-size: 24px; background-color:white" id="apiKey" disabled
+                        placeholder="*****"></input>
                 </div>
+                <button onclick="copyToClipboard('apiKey')" class="copy btn btn-dark form-control visually-hidden mt-2"
+                    id="clipboard">Copiar</button>
             </div>
         </div>
     </div>
 
 @stop
+
+<script>
+    function copyToClipboard(element_id) {
+        var aux = document.createElement("div");
+        aux.setAttribute("contentEditable", true);
+        aux.innerHTML = document.getElementById(element_id).value;
+        aux.setAttribute("onfocus", "document.execCommand('selectAll',false,null)");
+        document.body.appendChild(aux);
+        aux.focus();
+        document.execCommand("copy");
+        document.body.removeChild(aux);
+    }
+
+    $(document).ready(() => {
+        $("#generate").on('click', () => {
+            $.ajax({
+                method: "post",
+                url: "http://10.0.2.11:3000/dev/generate",
+                data: {
+                    "email": "{{ session()->get('user.email') }}"
+                }
+            }).done(res => {
+                $("#apiKey").val(res);
+                $("#clipboard").removeClass("visually-hidden");
+            })
+        })
+    })
+</script>
